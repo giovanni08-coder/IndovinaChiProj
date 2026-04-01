9f0add358342777cb5b9b8b2e5029907b2e1302b
giovanni08-coder <ferrari.giovannibattist.08@itisdalmine.edu.it>
2026-04-01 11:23:52 +0200

    inserisco modifiche rimaste dal merge

--- src/Personaggio.java
+++ src/Personaggio.java
@@ -1,84 +1,13 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 public class Personaggio {
-
+    private List<String> caratteristiche;
     private String nome;
-    private Map<String, String> caratteristiche;
-    private boolean attivo;
+    private String Url;
 
-    public Personaggio(String nome) {
-        this.nome=nome;
-        this.caratteristiche=new HashMap<>();
-        this.attivo=true;
+    public Personaggio(List<String> caratteristiche, String url, String nome) {
+        this.caratteristiche = caratteristiche;
+        Url = url;
+        this.nome = nome;
     }
-
-    public void aggiungiCaratteristica(String chiave, String valore) {
-        caratteristiche.put(chiave.toLowerCase(), valore.toLowerCase());
-    }
-
-    public String getCaratteristica(String chiave) {
-        return caratteristiche.get(chiave.toLowerCase());
-    }
-    public boolean haCaratteristica(String chiave) {
-        return caratteristiche.containsKey(chiave.toLowerCase());
-    }
-
-    // Solo per corregere o modificare i dati
-    public void rimuoviCaratteristica(String chiave) {
-        caratteristiche.remove(chiave.toLowerCase());
-    }
-
-    public boolean verificaCaratteristica(String chiave, String valore) {
-        String val = caratteristiche.get(chiave.toLowerCase());
-        if (val==null){
-            return false;
-        }
-        return val.equalsIgnoreCase(valore);
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public boolean isAttivo() {
-        return attivo;
-    }
-
-    public void setAttivo(boolean attivo) {
-        this.attivo = attivo;
-    }
-
-    @Override
-    public String toString() {
-        return nome + " | " + caratteristiche.toString();
-    }
-
-    public int confronta(Personaggio altro) {
-        int differenze = 0;
-        for (String chiave : caratteristiche.keySet()) {
-            String val1=caratteristiche.get(chiave);
-            String val2=altro.getCaratteristica(chiave);
-            if (val2==null || !val1.equals(val2)) {
-                differenze++;
-            }
-        }
-        return differenze;
-    }
-
-    // Differenze tra le caratteristiche
-    public Map<String, String> differenze(Personaggio altro) {
-        Map<String, String> diff = new HashMap<>();
-        for (String chiave : caratteristiche.keySet()) {
-            String val1=caratteristiche.get(chiave);
-            String val2=altro.getCaratteristica(chiave);
-            if (val2==null || !val1.equals(val2)) {
-                diff.put(chiave, val1);
-            }
-        }
-        return diff;
-    }
-
-    // TODO
-    // Inserire un metodo clone per salvare il personaggio da implementare poi nel Main
 }
