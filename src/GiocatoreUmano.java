3aa6400b02678ce11fb9f7e4f3525320fc6019d3
DenisSeru <serughetti.denis.08@itisdalmine.edu.it>
2026-04-07 20:26:25 +0200

    Classe GiocatoreUmano modificata (aggiunta anche la possibilità di fare una domanda)

--- src/GiocatoreUmano.java
+++ src/GiocatoreUmano.java
@@ -1,4 +1,6 @@
-public class GiocatoreUmano extends Giocatore{
+import javax.swing.JOptionPane;
+
+public class GiocatoreUmano extends Giocatore {
 
     public GiocatoreUmano(String nome, Albero albero, Personaggio[] tabellone) {
         super(nome, albero, tabellone);
@@ -6,6 +8,7 @@ public class GiocatoreUmano extends Giocatore{
 
     @Override
     public String ChiediDomanda() {
-
+        String domanda = JOptionPane.showInputDialog(null, "Fai una domanda sul personaggio", "Indovina Chi", JOptionPane.QUESTION_MESSAGE);
+        return domanda;
     }
 }
