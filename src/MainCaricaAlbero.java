ef39789833ada01407c359546e21d0fafb2a2b9d
giovanni08-coder <ferrari.giovannibattist.08@itisdalmine.edu.it>
2026-04-09 12:21:52 +0200

    continuo main dinamico

--- src/MainCaricaAlbero.java
+++ src/MainCaricaAlbero.java
@@ -124,9 +124,7 @@ void main() throws Exception {
     mappaCapelli.put("Ha i capelli biondi?",new ArrayList<>(Arrays.asList(personaggi[0],personaggi[8],personaggi[11],personaggi[13],personaggi[19])));
     mappaCapelli.put("Ha i capelli castani?",new ArrayList<>(Arrays.asList(personaggi[2],personaggi[3],personaggi[12],personaggi[14])));
     mappaCapelli.put("Ha i capelli bianchi?",new ArrayList<>(Arrays.asList(personaggi[1],personaggi[16],personaggi[23])));
-
-    Map<String,ArrayList<Personaggio>> mappaCalvo = new HashMap<>();
-    mappaCalvo.put("è calvo",new ArrayList<>(Arrays.asList(personaggi[4],personaggi[5],personaggi[9],personaggi[17],personaggi[18])));
+    mappaCapelli.put("è calvo?",new ArrayList<>(Arrays.asList(personaggi[4],personaggi[5],personaggi[9],personaggi[17],personaggi[18])));
 
     Map<String,ArrayList<Personaggio>> mappaOcchi = new HashMap<>();
     mappaOcchi.put("Ha gli occhi scuri?",new ArrayList<>(Arrays.asList(personaggi[1],personaggi[2],personaggi[3],personaggi[4],personaggi[6],personaggi[7],personaggi[8],personaggi[9],personaggi[10],personaggi[12],personaggi[13],personaggi[14],personaggi[15],personaggi[16],personaggi[17],personaggi[18],personaggi[19],personaggi[20],personaggi[22],personaggi[23])));
@@ -141,31 +139,26 @@ void main() throws Exception {
     mappaSesso.put("è un uomo?",new ArrayList<>(Arrays.asList(personaggi[2],personaggi[4],personaggi[5],personaggi[6],personaggi[7],personaggi[8],personaggi[9],personaggi[11],personaggi[13],personaggi[15],personaggi[16],personaggi[17],personaggi[18],personaggi[21],personaggi[22],personaggi[23])));
 
     Map<String,ArrayList<Personaggio>> mappaOrecchini = new HashMap<>();
-    mappaOrecchini.put("Ha gli orecchini",new ArrayList<>(Arrays.asList(personaggi[1],personaggi[12],personaggi[14],personaggi[19])));
+    mappaOrecchini.put("Ha gli orecchini?",new ArrayList<>(Arrays.asList(personaggi[1],personaggi[12],personaggi[14],personaggi[19])));
 
     Map<String,ArrayList<Personaggio>> mappaOcchiali = new HashMap<>();
-    mappaOcchiali.put("Ha gli occhiali",new ArrayList<>(Arrays.asList(personaggi[1],personaggi[4],personaggi[5],personaggi[16],personaggi[19],personaggi[20])));
+    mappaOcchiali.put("Ha gli occhiali?",new ArrayList<>(Arrays.asList(personaggi[1],personaggi[4],personaggi[5],personaggi[16],personaggi[19],personaggi[20])));
 
     Map<String,ArrayList<Personaggio>> mappaBarba = new HashMap<>();
-    mappaBarba.put("Ha il pizzetto o la barba",new ArrayList<>(Arrays.asList(personaggi[13],personaggi[16],personaggi[18],personaggi[15],personaggi[17])));
-    mappaBarba.put("Ha i baffi", new ArrayList<>(Arrays.asList(personaggi[4],personaggi[8],personaggi[21],personaggi[22])));
+    mappaBarba.put("Ha il pizzetto o la barba?",new ArrayList<>(Arrays.asList(personaggi[13],personaggi[16],personaggi[18],personaggi[15],personaggi[17])));
+    mappaBarba.put("Ha i baffi?", new ArrayList<>(Arrays.asList(personaggi[4],personaggi[8],personaggi[21],personaggi[22])));
 
     Map<String,ArrayList<Personaggio>> mappaCappello = new HashMap<>();
-    mappaCappello.put("Ha il cappello", new ArrayList<>(Arrays.asList(personaggi[2],personaggi[6],personaggi[7],personaggi[14],personaggi[19])));
-
-    Map<String,ArrayList<Personaggio>> mappaCapelliLunghi = new HashMap<>();
-    mappaCapelliLunghi.put("Ha i capelli lunghi",new ArrayList<>(Arrays.asList(personaggi[4],personaggi[10],personaggi[20])));
+    mappaCappello.put("Ha il cappello?", new ArrayList<>(Arrays.asList(personaggi[2],personaggi[6],personaggi[7],personaggi[14],personaggi[19])));
 
     // Poi inseriscila nella mappa esterna
-    genere.put("capelli", mappaCapelli);
-    genere.put("Calvo",mappaCalvo);
-    genere.put("occhi",mappaOcchi);
+    genere.put("Capelli", mappaCapelli);
+    genere.put("Occhi",mappaOcchi);
     genere.put("Pelle",mappaPelle);
     genere.put("Sesso",mappaSesso);
     genere.put("Orecchini",mappaOrecchini);
     genere.put("Occhiali",mappaOcchiali);
     genere.put("Barba",mappaBarba);
     genere.put("Capello",mappaCappello);
-    genere.put("capelli lunghi", mappaCapelliLunghi);
     GestoreFile.Scrivi_binariDomande(genere);
 }
--- src/MainDinamico.java
+++ src/MainDinamico.java
@@ -1,6 +1,15 @@
 void main() throws Exception {
     Map<String, Map<String, ArrayList<Personaggio>>> GeneriDomande = GestoreFile.Leggi_binarioDomande();
     Personaggio[] personaggi = GestoreFile.Leggi_binarioPersonaggi();
-    System.out.println(GeneriDomande.get("capelli").get("Ha i capelli biondi?"));
-    System.out.println(GeneriDomande.get("capelli").get("Ha i capelli biondi?").get(0).equals(personaggi[0]));
+
+    List<String> ChiaviGeneri = new ArrayList<>(GeneriDomande.keySet());
+    System.out.println(ChiaviGeneri);
+
+    Random r = new Random();
+
+    List<String> ChiaviDomande =new ArrayList<>(GeneriDomande.get(ChiaviGeneri.get(r.nextInt(ChiaviGeneri.size()))).keySet());
+    System.out.println(ChiaviDomande);
+
+    System.out.println(ChiaviDomande.get(r.nextInt(ChiaviDomande.size())));
+
 }
